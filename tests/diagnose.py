c18e3c479b9ceb331551846f5e9c8cff45dfc41b
f73chen <grace.fenglin@gmail.com>
2020-07-29 18:47:38 -0400

    Create diagnose.py

---
+++ tests/diagnose.py
@@ -0,0 +1,11 @@
+import WDL
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("-i", "--input-wdl-path", required = True, help = "source wdl path")
+args = parser.parse_args()
+doc = WDL.load(args.input_wdl_path)     # loads the file as a WDL.Tree.Document object
+
+print("required inputs:")
+for item in doc.workflow.required_inputs:
+    input = item.value
+    print(str(input.type), str(input.name))
